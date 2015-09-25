287ccfbfd46161f8167f4a4fccd088f9a3cd1d6b
Mark Frost <cheerful.mf@gmail.com>
2015-09-25 13:57:40 +0500

    added tests for tours_srcs

--- spec/lib/services/tours_spec.rb
+++ spec/lib/services/tours_spec.rb
@@ -12,8 +12,25 @@ describe Rosstour::Services::Tours do
       expect( rosstour.tours_srcs ).to be_a Rosstour::TourSrc
     end
 
+    describe 'returned array' do
+      it 'should not be empty' do
+        expect( rosstour.tours_srcs ).not_to be_empty
+      end
+
+      it 'should consist of documents' do
+        all_documents = rosstour.tours_srcs.all? do |document|
+          document.is_a? Rosstour::TourSrc
+        end
+        expect( all_documents ).to be true
+      end
+    end
+    
+    describe 'returned document' do
+      let(:document) {rosstour.tours_srcs.first}
+      it 'should have Id field' do
+        expect( document ).to respond_to :id
+        expect( document ).not_to be nill
+      end
+    end
   end
-
-
-
 end
